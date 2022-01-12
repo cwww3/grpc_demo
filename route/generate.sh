c05d31f38ba44f1340695a406bc108331d9abd8d
cwww3 <2239354893@qq.com>
2022-01-12 16:44:42 +0800

    更新

--- route/generate.sh
+++ route/generate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # 在当前目录运行
 # 由于生成grpc-gateway和openapi需要导入第三方的proto文件(下载并保存在本地)
+# 下载地址 https://github.com/googleapis/googleapis
+
 # 通过buf可能直接使用远程的proto文件,不需要下载到本地
 protoc --go_out=. --go_opt=paths=source_relative \
 --go-grpc_out=. --go-grpc_opt=paths=source_relative \
